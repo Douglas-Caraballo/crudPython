3a22c665c3949a595278684774e48851e1b750bd
Doug <karaballo.douglas@hotmail.com>
2022-10-21 08:53:54 -0400

    Finalizado la interface grafica

--- paytonCrud.py
+++ paytonCrud.py
@@ -29,10 +29,70 @@ barraMenu.add_cascade(label="Ayuda", menu=ayudaMenu)
 frameCampos = Frame(raiz)
 frameCampos.pack()
 
+miID = StringVar()
+miNombre = StringVar()
+miApellido = StringVar()
+miPass = StringVar()
+miDireccion = StringVar()
 
+cuadroID = Entry(frameCampos, textvariable=miID)
+cuadroID.grid(row=0, column=1, padx=10, pady=10)
 
+cuadroNombre = Entry(frameCampos, textvariable=miNombre)
+cuadroNombre.grid(row=1, column=1, padx=10, pady=10)
 
+cuadroApellido = Entry(frameCampos, textvariable=miApellido)
+cuadroApellido.grid(row=2, column=1, padx=10, pady=10)
 
+cuadroPass = Entry(frameCampos, textvariable=miPass)
+cuadroPass.grid(row=3, column=1, padx=10, pady=10)
+cuadroPass.config(show="?")
 
+cuadroDireccion = Entry(frameCampos, textvariable=miDireccion)
+cuadroDireccion.grid(row=4, column=1, padx=10, pady=10)
+
+textoComentario = Text(frameCampos, width=16, height=5)
+textoComentario.grid(row=5, column=1,padx=10, pady=10)
+scrollVert = Scrollbar(frameCampos,command=textoComentario.yview)
+scrollVert.grid(row=5,column=2,sticky="nsew")
+
+textoComentario.config(yscrollcommand=scrollVert.set)
+
+#-----------------------Labels---------------------------------------
+
+idLabel = Label(frameCampos, text="ID")
+idLabel.grid(row=0,column=0,sticky="e", padx=10, pady=10)
+
+nombreLabel = Label(frameCampos,text="Nombre")
+nombreLabel.grid(row=1,column=0, sticky="e", padx=10, pady=10)
+
+apellidoLabel = Label(frameCampos, text="Apellido")
+apellidoLabel.grid(row=2, column=0, sticky="e", padx=10, pady=10)
+
+passLabel = Label(frameCampos, text="Contraseña")
+passLabel.grid(row=3, column=0, sticky="e", padx=10, pady=10)
+
+direccionLabel = Label(frameCampos, text="Dirección")
+direccionLabel.grid(row=4, column=0, sticky="e", padx=10, pady=10)
+
+comentarioLabel = Label(frameCampos, text="Comentarios")
+comentarioLabel.grid(row=5, column=0, sticky="e", padx=10, pady=10)
+
+#------------------------Botones--------------------------------------
+
+frameBotones = Frame(raiz)
+frameBotones.pack()
+
+botonCrear = Button(frameBotones, text= "Crear")
+botonCrear.grid(row=1, column=0, sticky="e", padx=10, pady=10)
+
+botonLeer = Button(frameBotones, text= "Buscar")
+botonLeer.grid(row=1, column=1, sticky="e", padx=10, pady=10 )
+
+botonActualizar = Button(frameBotones, text= "Actualizar")
+botonActualizar.grid(row=1, column=2, sticky="e", padx=10, pady=10)
+
+botonEliminar = Button(frameBotones, text="Eliminar")
+botonEliminar.grid(row=1, column=4, sticky="e", padx=10, pady=10)
 
 raiz.mainloop()
